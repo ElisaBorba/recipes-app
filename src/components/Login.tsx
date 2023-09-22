c94917159180fca4db1840377b62207e7e5d75bc
amandagiussani <amanda.giussani@hotmail.com>
2023-09-21 21:31:20 -0300

    requisitos atualizados

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Login() {
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    validateForm(e.target.value, password);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+    validateForm(email, e.target.value);
+  };
+
+  const validateForm = (emailValue: string, passwordValue: string) => {
+    const isEmailValid = /\S+@\S+\.\S+/.test(emailValue);
+    const isPasswordValid = passwordValue.length > 6;
+    setFormIsValid(isEmailValid && isPasswordValid);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Enviar o formulário se for válido
+    if (formIsValid) {
+      // Lógica de envio do formulário aqui
+
+      // Salvar o e-mail no localStorage
+      const userData = { email };
+      localStorage.setItem('user', JSON.stringify(userData));
+    }
+  };
+
   return (
     <div>
-      <h1>Login</h1>
+      <h2>Login</h2>
+      <form onSubmit={ handleSubmit }>
+        <label htmlFor="email">E-mail</label>
+        <input
+          type="email"
+          id="email"
+          data-testid="email-input"
+          placeholder="Digite seu e-mail"
+          value={ email }
+          onChange={ handleEmailChange }
+        />
+
+        <label htmlFor="password">Senha</label>
+        <input
+          type="password"
+          id="password"
+          data-testid="password-input"
+          placeholder="Digite sua senha"
+          value={ password }
+          onChange={ handlePasswordChange }
+        />
+
+        <button type="submit" data-testid="login-submit-btn" disabled={ !formIsValid }>
+          Entrar
+        </button>
+      </form>
     </div>
   );
 }
+
+export default Login;
