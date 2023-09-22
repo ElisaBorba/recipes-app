e885ff763267cdf5865aa6e9209ae7cad3fc5942
ElisaBorba <elisaborbaa@gmail.com>
2023-09-22 17:13:06 -0300

    implementa 8 e 9 mas falta os testes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import ProfileIcon from '../images/profileIcon.svg';
 import SearchIcon from '../images/searchIcon.svg';
 
@@ -9,23 +10,57 @@ type HeaderType = {
 };
 
 export default function Header({ title, isProfile, isSearch }: HeaderType) {
+  const [searchBar, setSearchBar] = useState('');
+  const [showSearchBar, setShowSearchBar] = useState(false);
+
+  const handleClickSearchBar = () => {
+    setShowSearchBar(true);
+
+    if (showSearchBar === true) {
+      setShowSearchBar(false);
+    }
+  };
+
+  const handleSearchBar = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchBar(e.target.value);
+  };
+
   return (
     <>
       {isProfile && (
-        <img
-          data-testid="profile-top-btn"
-          src={ ProfileIcon }
-          alt="Ícone de Perfil"
-        />
+        <NavLink to="/profile">
+          <img data-testid="profile-top-btn" src={ ProfileIcon } alt="Ícone de Perfil" />
+        </NavLink>
       )}
+
       {isSearch && (
-        <img
-          data-testid="search-top-btn"
-          src={ SearchIcon }
-          alt="ícone de Procura"
-        />
+        <button
+          onClick={ handleClickSearchBar }
+        >
+          <img
+            data-testid="search-top-btn"
+            src={ SearchIcon }
+            alt="Ícone de Procura"
+          />
+        </button>
       )}
       <h1 data-testid="page-title">{title}</h1>
+
+      {showSearchBar
+      && (
+        <form>
+          <label htmlFor="searchBar">
+            <input
+              type="text"
+              id="searchBar"
+              value={ searchBar }
+              onChange={ handleSearchBar }
+              data-testid="search-input"
+              placeholder="Search"
+            />
+          </label>
+        </form>
+      )}
     </>
   );
 }
