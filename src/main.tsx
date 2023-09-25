22c6cfc3fdab32c41e0d2e73050901f5b27c8f67
ElisaBorba <elisaborbaa@gmail.com>
2023-09-25 19:51:24 -0300

    arruma os providers para o main

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import DrinksProvider from './context/DrinksContext/DrinksProvider';
+import MealsProvider from './context/MealsContext/MealsProvider';
 
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLElement)
   .render(
     <BrowserRouter>
-      <App />
+      <MealsProvider>
+        <DrinksProvider>
+          <App />
+        </DrinksProvider>
+      </MealsProvider>
     </BrowserRouter>,
   );
