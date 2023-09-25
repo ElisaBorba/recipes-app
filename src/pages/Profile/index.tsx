10ef6008c76bdaa80cf13850ecdaf37a94614622
josypamplona <josypamplona@hotmail.com>
2023-09-25 17:47:38 -0300

    req 16-17-18  ok

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
 export default function Profile() {
-  const [ loading, setLoadin] = useState<boolean>(true)
+  const [ loading, setLoadin] = useState<boolean>(true);
   return (
     <>
       <header>
