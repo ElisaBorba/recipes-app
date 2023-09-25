6710a7e261979e7c954fc542b09c74486d7f316c
josypamplona <josypamplona@hotmail.com>
2023-09-25 18:33:49 -0300

    req 16-17-18

--- src/context/BeerContext.tsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-import { UserContextType } from '../@type/ContextType';
-
-const bebidas = createContext<UserContextType>({} as UserContextType);
-
-export default bebidas;
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -3,7 +3,6 @@ import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
 export default function Profile() {
-  const [ loading, setLoadin] = useState<boolean>(true);
   return (
     <>
       <header>
