ad117070a4253ae0c0cf26080ff43c3220f602a9
flaubertemadeiro <flaubertemadeiro@gmail.com>
2023-09-26 16:04:39 -0300

    requisito 10 ao 15 completo

--- src/tests/SearchBar.test.tsx
+++ src/tests/SearchBar.test.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import userEvent from '@testing-library/user-event';
 import SearchBar from '../components/SearchBar';
 import { DataProvider } from '../context/dataprovider';
+import { renderWithRouter } from './utils/renderWithRouter';
+import App from '../App';
 
 describe('Verifica se o componente SearchBar existe e se funciona como o esperado', () => {
   test('Renderiza o componente SearchBar e verifica a seleção de radio buttons', () => {
     render(
-      <DataProvider>
-        <SearchBar title="Meals" />
-      </DataProvider>,
+      <MemoryRouter>
+        <DataProvider>
+          <SearchBar title="Meals" />
+        </DataProvider>
+      </MemoryRouter>,
     );
 
     // Verifica se os elementos de radio estão renderizados e inicia com "Ingredient" selecionado.
@@ -32,5 +38,46 @@ describe('Verifica se o componente SearchBar existe e se funciona como o esperad
     expect(nameRadio).not.toBeChecked();
 
     fireEvent.click(button);
+    expect(firstLetterRadio).toBeChecked();
+
+    fireEvent.click(ingredientRadio);
+    expect(ingredientRadio).toBeChecked();
+    expect(firstLetterRadio).not.toBeChecked();
+
+    fireEvent.click(button);
+    expect(ingredientRadio).toBeChecked();
+
+    fireEvent.click(nameRadio);
+    expect(nameRadio).toBeChecked();
+    expect(ingredientRadio).not.toBeChecked();
+
+    fireEvent.click(button);
+    expect(nameRadio).toBeChecked();
+
+    fireEvent.click(firstLetterRadio);
+    expect(firstLetterRadio).toBeChecked();
+    expect(nameRadio).not.toBeChecked();
+
+    fireEvent.click(button);
+    expect(firstLetterRadio).toBeChecked();
+
+    // Verifica se o botão de execução de busca está renderizado.
+    expect(button).toBeInTheDocument();
+  });
+  it('Verificar as buscas de drinks', async () => {
+    renderWithRouter(<DataProvider><App /></DataProvider>, { route: '/drinks' });
+    const button = screen.getByRole('img', {
+      name: /ícone de procura/i,
+    });
+    fireEvent.click(button);
+    const inputSearch: HTMLInputElement = screen.getByPlaceholderText(/search/i);
+    expect(inputSearch).toBeInTheDocument();
+
+    const searchButton = screen.getByRole('button', {
+      name: /search/i,
+    });
+
+    await userEvent.type(inputSearch, 'gin');
+    await userEvent.click(searchButton);
   });
 });
