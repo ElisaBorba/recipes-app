f991b4d7a9f4fb9e219198380728808bd4574487
amandagiussani <amanda.giussani@hotmail.com>
2023-09-27 14:52:27 -0300

    requisitos profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,14 +1,47 @@
-import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Header from '../../components/Header';
-import Footer from '../../components/Footer';
 
-export default function Profile() {
+function Profile() {
+  const emailUser = localStorage.getItem('user');
+  console.log(emailUser);
+
+  const navigate = useNavigate();
+  const handleClickRecipes = () => {
+    navigate('/done-recipes');
+  };
+
+  const handleClickFavorite = () => {
+    navigate('/favorite-recipes');
+  };
+
+  const handleClickLogout = () => {
+    localStorage.clear();
+    navigate('/');
+  };
+
   return (
     <>
-      <header>
-        <Header title="Profile" isProfile isSearch={ false } />
-      </header>
-      <Footer />
+      <Header title="Profile" isProfile={ false } isSearch={ false } />
+      <h2 data-testid="profile-email">{ emailUser }</h2>
+      <button
+        data-testid="profile-done-btn"
+        onClick={ handleClickRecipes }
+      >
+        Done Recipes
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        onClick={ handleClickFavorite }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        onClick={ handleClickLogout }
+      >
+        Logout
+      </button>
     </>
   );
 }
+export default Profile;
