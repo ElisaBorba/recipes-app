13fc3fd665c7e034a7e0400b7a9269ab797345d2
flaubertemadeiro <flaubertemadeiro@gmail.com>
2023-09-28 11:29:24 -0300

    requisito 38 feito: aplicando checkbox

--- src/components/RecipeInProgress.tsx
+++ src/components/RecipeInProgress.tsx
@@ -58,6 +58,28 @@ function RecipeInProgress() {
       <h3>Instruções:</h3>
       <p data-testid="instructions">{strInstructions}</p>
 
+      <h3>Ingredientes:</h3>
+      <ul>
+        {Object.keys(recipe).map((key) => {
+          if (key.startsWith('strIngredient') && recipe[key]) {
+            const ingredientIndex = key.replace('strIngredient', '');
+            const measureKey = `strMeasure${ingredientIndex}`;
+            return (
+              <li key={ ingredientIndex }>
+                <label
+                  data-testid={ (
+                     `${parseInt(ingredientIndex, 10) - 1}-ingredient-step`) }
+                >
+                  <input type="checkbox" />
+                  {`${recipe[key]} - ${recipe[measureKey]}`}
+                </label>
+              </li>
+            );
+          }
+          return null;
+        })}
+      </ul>
+
       <button data-testid="share-btn">Compartilhar</button>
       <button data-testid="favorite-btn">Favoritar</button>
       <button data-testid="finish-recipe-btn">Finalizar Receita</button>
