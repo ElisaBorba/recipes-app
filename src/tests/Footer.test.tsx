bdf1c8842d93fa651ad49d9ee1bd3af7013d092a
ElisaBorba <elisaborbaa@gmail.com>
2023-09-29 14:45:07 -0300

    req 18 da josy

--- src/tests/Footer.test.tsx
+++ src/tests/Footer.test.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-// import Meals from '../pages/Meals';
-import Footer from '../components/Footer';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import { renderWithRouter } from './utils/renderWithRouter';
+import { DataProvider } from '../context/dataprovider';
 
-test('Se tem dois icones em footer', () => {
-  const { getByTestId } = render(<Footer />);
-  const primeiroIcone = getByTestId('drinks-bottom-btn');
-  const segundoIcone = getByTestId('meals-bottom-btn');
-  expect(primeiroIcone).toBeInTheDocument();
-  expect(segundoIcone).toBeInTheDocument();
+describe('Verifica as funcionalidades do componente Footer', () => {
+  it('Se tem dois ícones em footer', () => {
+    renderWithRouter(<DataProvider><App /></DataProvider>, { route: '/meals' });
+
+    const drinkIcon = screen.getByTestId('drinks-bottom-btn');
+    const mealIcon = screen.getByTestId('meals-bottom-btn');
+    expect(drinkIcon).toBeInTheDocument();
+    expect(mealIcon).toBeInTheDocument();
+  });
+
+  it('Se os ícones redirecionam para a página correta', async () => {
+    renderWithRouter(<DataProvider><App /></DataProvider>, { route: '/meals' });
+
+    const drinkIcon = screen.getByTestId('drinks-bottom-btn');
+    await userEvent.click(drinkIcon);
+    expect(window.location.pathname).toBe('/drinks');
+  });
+
+  it('Se os ícones redirecionam para a página correta', async () => {
+    renderWithRouter(<DataProvider><App /></DataProvider>, { route: '/drinks' });
+
+    const mealIcon = screen.getByTestId('meals-bottom-btn');
+    await userEvent.click(mealIcon);
+    expect(window.location.pathname).toBe('/meals');
+  });
 });
