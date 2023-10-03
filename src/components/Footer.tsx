88ce3d79993ef16b68b544f8bd7d9f840eada8bc
ElisaBorba <elisaborbaa@gmail.com>
2023-10-03 14:26:05 -0300

    faz o footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,30 @@
 import { Link } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
-import './Footer.css';
+import styles from './Footer.module.css';
 
 export default function Footer() {
   return (
-    <footer className="footer" data-testid="footer">
+    <footer className={ styles.footer } data-testid="footer">
       <Link
         to="/drinks"
       >
-        <img data-testid="drinks-bottom-btn" src={ drinkIcon } alt={ drinkIcon } />
+        <img
+          className={ styles.icons }
+          data-testid="drinks-bottom-btn"
+          src={ drinkIcon }
+          alt={ drinkIcon }
+        />
       </Link>
-
       <Link
         to="/meals"
       >
-        <img data-testid="meals-bottom-btn" src={ mealIcon } alt={ drinkIcon } />
+        <img
+          className={ styles.icons }
+          data-testid="meals-bottom-btn"
+          src={ mealIcon }
+          alt={ drinkIcon }
+        />
       </Link>
     </footer>
   );
